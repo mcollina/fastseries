a6e03d7ca7ad33ba894813d0a51d941980d3bb01
Matteo Collina <hello@matteocollina.com>
2016-05-23 18:06:07 +0200

    added tests for empty each.

--- test.js
+++ test.js
@@ -278,3 +278,36 @@ test('call without arg if there is no arg with results', function (t) {
     })
   }
 })
+
+test('each support with nothing to process', function (t) {
+  t.plan(2)
+
+  var instance = series()
+  var obj = {}
+  var args = []
+
+  instance(obj, something, args, function done (err, results) {
+    t.error(err)
+    t.deepEqual(results, [], 'empty results')
+  })
+
+  function something (arg, cb) {
+    t.fail('this should never happen')
+  }
+})
+
+test('each without results support with nothing to process', function (t) {
+  t.plan(1)
+
+  var instance = series({ results: false })
+  var obj = {}
+  var args = []
+
+  instance(obj, something, args, function done () {
+    t.pass('done called')
+  })
+
+  function something (arg, cb) {
+    t.fail('this should never happen')
+  }
+})
